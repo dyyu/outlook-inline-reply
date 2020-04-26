59181a36662334a8ab78b1d1de08dce2d77c804b
dyyu <8497440+dyyu@users.noreply.github.com>
2020-04-27 01:39:57 +0200

    Added email reformatting to insert quotation bar before previous message. Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# outlook-inline-reply
+# Outlook Inline Reply
+
+This is an add-in for Outlook 365 that provides a traditional style inline reply.
+Currently, it supports Outlook Desktop and Outlook on the
+
+# Quick Start
+
+To run this add-in on your own machine, take the following steps.
+
+0.  Make sure you satisfy the prerequisites and clone this repository.
+    -   Node.js
+    
+1.  Start the local server.
+
+        npm start
+
+2.  Sideload the add-in to your Outlook.
+
+3.  Try inline replying.
+
+    1.  Randomly select an email you received and click on reply.
+
+    2.  In the compose window, click the three dots to expand the email history.
+    
+    3.  Find the add-in Inline Reply in the menu and click on "Reformat Email."
+
+    4.  You should see that there is a vertical blockquote bar before the email history.
--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,31 +1,73 @@
 /*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * Original work Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * Modified work Copyright dyyu. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
-
 /* global global, Office, self, window */
 
+// The initialize function must be run each time a new page is loaded.
+Office.initialize = function (reason) {
+  self.console.info('Office.initialize: ' + reason);
+};
+
 Office.onReady(() => {
+  self.console.info('Office.onReady');
   // If needed, Office.js is ready to be called
 });
 
 /**
  * Shows a notification when the add-in command is executed.
- * @param event
+ * @param event {Office.AddinCommands.Event}
  */
-function action(event: Office.AddinCommands.Event) {
-  const message: Office.NotificationMessageDetails = {
+function reformatEmail(event) {
+  var item = Office.context.mailbox.item;
+
+  // Check whether the body is plaintext or html
+  item.body.getTypeAsync(function(result) {
+    var emailFormat = result.value;
+    self.console.info('Email format is', emailFormat);
+    
+    // Get the current body (this does not include the message history if it's hidden)
+    item.body.getAsync(emailFormat, function(result){
+      self.console.info("Get email body", result.status);
+
+      // Do nothing if the there's an error getting the body
+      if(result.status == Office.AsyncResultStatus.Failed) event.completed();
+
+      // TODO: Replace the quoted email header with a single line:
+      // E.g., On Thursday, April 23, 2020 at 4:15 PM Jane <jane@protonmail.ch> wrote:
+
+      // Add the quotes
+      if(emailFormat == Office.CoercionType.Html) {
+        // Add the blockquote tag
+        var blockquoteTag = "<blockquote class=\"x_gmail_quote\" style=\"margin:0px 0px 0px 0.8ex; border-left:1px solid rgb(204,204,204); padding-left:1ex\">";
+        var newBody = blockquoteTag + result.value + "</blockquote>";
+
+      } else if(emailFormat == Office.CoercionType.Text) {
+        // TODO: handle plain text email
+      }
+
+      // Write to the new body
+      item.body.setAsync(newBody, {coercionType: emailFormat}, function(result){
+        self.console.info("Set email body", result.status);
+
+        // Show a notification message
+        item.notificationMessages.replaceAsync("reformatEmail", notificationMessage("Email is now reformatted for inline reply."));
+
+        // Be sure to indicate when the add-in command function is complete
+        event.completed();  
+      })
+    })
+  });
+}
+
+function notificationMessage(message) {
+  return {
     type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-    message: "Performed action.",
+    message: message,
     icon: "Icon.80x80",
-    persistent: true
+    persistent: false
   };
-
-  // Show a notification message
-  Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
-
-  // Be sure to indicate when the add-in command function is complete
-  event.completed();
 }
 
 function getGlobal() {
@@ -41,4 +83,4 @@ function getGlobal() {
 const g = getGlobal() as any;
 
 // the add-in command functions need to be available in global scope
-g.action = action;
+g.reformatEmail = reformatEmail;
